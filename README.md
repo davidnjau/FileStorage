c9866ce6bff72eb4cdc567bbbe33370014fe86a1
Dave <davidnjau21@gmail.com>
2022-08-26 07:34:19 +0300

    add swagger to project

---
+++ README.md
@@ -0,0 +1,36 @@
+# Smsrevamp
+
+[![Build Status](https://travis-ci.org/codecentric/springboot-sample-app.svg?branch=master)](https://travis-ci.org/codecentric/springboot-sample-app)
+[![Coverage Status](https://coveralls.io/repos/github/codecentric/springboot-sample-app/badge.svg?branch=master)](https://coveralls.io/github/codecentric/springboot-sample-app?branch=master)
+[![License](http://img.shields.io/:license-apache-blue.svg)](http://www.apache.org/licenses/LICENSE-2.0.html)
+
+SmsManager [Spring Boot](http://projects.spring.io/spring-boot/).
+
+## Requirements
+
+For building and running the application you need:
+
+- [JDK 1.8](http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html)
+- [Maven 3](https://maven.apache.org)
+- [Postgresql](https://https://www.postgresql.org/)
+
+## Running the application locally
+
+```shell
+mvn spring-boot:run
+```
+## Accessing Swagger
+
+You can access the swagger UI platform from:
+
+```shell
+http://{{host_url}}:8081/swagger-ui.html
+```
+
+
+
+
+
+## Copyright
+
+Released under the Apache License 2.0. See the [LICENSE](https://github.com/codecentric/springboot-sample-app/blob/master/LICENSE) file.
--- src/main/java/com/dave/filestorage/controller/FilesController.java
+++ src/main/java/com/dave/filestorage/controller/FilesController.java
@@ -3,6 +3,12 @@ package com.dave.filestorage.controller;
 import com.dave.filestorage.model.Files;
 import com.dave.filestorage.payload.FileUploadResponse;
 import com.dave.filestorage.service.FileStorageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
@@ -13,18 +19,26 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+@Tag(description = "Use these resource to save file in the system.", name = "File system module")
 @RestController
 public class FilesController {
 
     @Autowired
     private FileStorageService fileStorageService;
 
-    @PostMapping("/fileupload")
+    @Operation(summary = "Upload a File", description = "Uploads a file to the server's file system.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "${api.response-codes.ok.desc}"),
+            @ApiResponse(responseCode = "400", description = "${api.response-codes.badRequest.desc}",
+                    content = { @Content(examples = { @ExampleObject(value = "") }) }),
+            @ApiResponse(responseCode = "404", description = "${api.response-codes.notFound.desc}",
+                    content = { @Content(examples = { @ExampleObject(value = "") }) }) })
+    @PostMapping("/uploadFile")
     public FileUploadResponse uploadFile(@RequestParam("file") MultipartFile file) {
         Files files = fileStorageService.storeFile(file);
 
         String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
-                .path("/filedownload/")
+                .path("/download-file/")
                 .path(files.getId())
                 .toUriString();
 
@@ -32,7 +46,14 @@ public class FilesController {
                 file.getContentType(), file.getSize());
     }
 
-    @GetMapping("/filedownload/{fileId}")
+    @Operation(summary = "Download a File", description = "Dowload a file to the server's file system.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "${api.response-codes.ok.desc}"),
+            @ApiResponse(responseCode = "400", description = "${api.response-codes.badRequest.desc}",
+                    content = { @Content(examples = { @ExampleObject(value = "") }) }),
+            @ApiResponse(responseCode = "404", description = "${api.response-codes.notFound.desc}",
+                    content = { @Content(examples = { @ExampleObject(value = "") }) }) })
+    @GetMapping("/download-file/{fileId}")
     public ResponseEntity<Resource> downloadFile(@PathVariable String fileId) {
         Files dbFile = fileStorageService.getFile(fileId);
 
